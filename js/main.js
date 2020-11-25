6c6f16ff38642bb2ef9534e2f65c8a3c77913e83
ivanpirat <66178985+ivanpirat@users.noreply.github.com>
2020-11-26 00:10:08 +0300

    test3js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-document.getElementById('h1').innerHTML = 12312312312;
+ 
+ var clicks =1 ;
+
+
+  function contact_info()
+    {
+     if(clicks == 1)
+      {
+        clicks -= 1;
+        document.getElementById("clicks").innerHTML = clicks;
+      document.getElementById('contact_info1').style.display="none"
+           document.getElementById('contact_info2').style.display="block"
+      }
+   else if (clicks == 0)
+      {
+     document.getElementById("clicks").innerHTML = clicks;
+      clicks += 1;
+    document.getElementById('contact_info2').style.display="none"
+        document.getElementById('contact_info1').style.display="block"
+        
+         
+      }
+
+     
+
+
+  }
